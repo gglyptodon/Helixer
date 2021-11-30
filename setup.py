50888e74cfa0d085b1de6160b50a30bc39b59291
Janina Mass <janina.mass@gmail.com>
2021-11-30 10:43:15 +0100

    add helixer.core to packages

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
    name='helixer',
    version='0.2.0',
    description='Deep Learning fun on gene structure data',
-   packages=['helixer', 'helixer.prediction', 'helixer.evaluation', 'helixer.tests', 'helixer.export'],  #same as name
+   packages=['helixer', 'helixer.prediction', 'helixer.evaluation', 'helixer.tests', 'helixer.core','helixer.export'],  #same as name
    package_data={'helixer': ['testdata/*.fa', 'testdata/*.gff']},
    install_requires=["geenuff @ https://github.com/weberlab-hhu/GeenuFF/archive/refs/heads/main.zip"],
 dependency_links=["https://github.com/weberlab-hhu/GeenuFF/archive/refs/heads/main.zip#egg=geenuff"],
